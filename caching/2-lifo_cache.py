7018b87ebfcc01387e8fabd56766b5fc5336faa6
bz888 <ziyu.ben158@gmail.com>
2026-07-27 23:34:35 +1000

    2

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-"""FIFO caching module."""
+"""LIFO caching module."""
 
 from base_caching import BaseCaching
 
 
-class FIFOCache(BaseCaching):
-    """FIFO caching system with a storage limit."""
+class LIFOCache(BaseCaching):
+    """LIFO caching system with a storage limit."""
 
     def put(self, key, item):
-        """Add an item and discard the oldest item when the cache is full."""
+        """Add an item and discard the most recently added item when the cache is full."""
         if key is None or item is None:
             return
 
-        self.cache_data[key] = item
-
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key = next(iter(self.cache_data))
+            first_key = next(reversed(self.cache_data))
             del self.cache_data[first_key]
             print("DISCARD: {}".format(first_key))
+            
+        self.cache_data[key] = item
 
     def get(self, key):
         """Return the cached value for key, or None when it is missing."""
