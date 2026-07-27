d18692cf8290af688617a06e7fec89e822c0cdef
bz888 <ziyu.ben158@gmail.com>
2026-07-27 23:00:19 +1000

    1

---
+++ caching/1-fifo_cache.py
@@ -0,0 +1,20 @@
+
+from base_caching import BaseCaching
+
+class FIFOCache(BaseCaching):
+    """FIFO caching system with a storage limit."""
+    
+    def put(self, key, item):
+        """Add an item to the cache when its key and value are not None.
+		If the cache exceeds the maximum size, remove the first item added."""
+        if key is not None and item is not None:
+            if len(self.cache_data) >self.MAX_ITEMS:
+                first = next(iter(self.cache_data))
+                del self.cache_data[first]
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Return the cached value for key, or None when it is missing."""
+        if key is not None:
+            return self.cache_data.get(key)
+        return None
