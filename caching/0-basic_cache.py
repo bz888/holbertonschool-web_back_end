a1caecfc7474f50bfd2fbac7292c131799ae2d7e
bz888 <ziyu.ben158@gmail.com>
2026-07-27 22:45:42 +1000

    add 1

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,14 @@
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    
+    def put(self, key, item):
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        if key is not None:
+            return self.cache_data.get(key)
+        return None
