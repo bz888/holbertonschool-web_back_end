7d3f15e0fd3931f5af5d3ef3a7692fa5574d2149
bz888 <ziyu.ben158@gmail.com>
2026-08-02 20:06:30 +1000

    typing added

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """Yield 10 random numbers, waiting one second before each yield."""
     for _ in range(10):
         await asyncio.sleep(1)
--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Collect values from an asynchronous generator."""
 
+from typing import List
+
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """Return 10 values collected with an async comprehension."""
     return [number async for number in async_generator()]
