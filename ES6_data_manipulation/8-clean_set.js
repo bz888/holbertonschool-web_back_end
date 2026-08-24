90fc20e9ca8fec1f602920a118dc65ef5fd11276
bz888 <ziyu.ben158@gmail.com>
2026-08-24 20:48:53 +1000

    89

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,6 @@
 export default function cleanSet(set, startString) {
+  if (startString === undefined) return '';
+
   return [...set]
     .filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length))
